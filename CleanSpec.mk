2d18037e2174ee7b7813fc33a1b6e434ae0bda54
Marc Obrador <marc.obrador@gi-de.com>
2015-01-13 17:32:02 +0100

    Fix CleanSpec.mk

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,3 +1,2 @@
-$(call add-clean-step, rm -f $(PRODUCT_OUT)/system/app/AssdTerminal.apk)
+$(call add-clean-step, rm -f $(PRODUCT_OUT)/system/app/AssdTerminal/AssdTerminal.apk)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/APPS/AssdTerminal_intermediates)
-
